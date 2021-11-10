38d261a23ac50e206042ff224e731965e422a755
wenjj <jjwen@mae.cuhk.edu.hk>
2021-11-10 00:50:01 -0800

    Add env without normalization
    Signed-off-by: wenjj <jjwen@mae.cuhk.edu.hk>

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 import yaml
 import importlib
-
+import matplotlib.pyplot as plt
 
 
 def load_environments(config):
@@ -18,20 +18,28 @@ def load_environments(config):
 
 
 if __name__ == '__main__':
-    env = load_environments("configs/IntersectionEnv/env.json")
+    env = load_environments("configs/IntersectionEnv/env_no_normalize.json")
     done = False
     for ep in range(50):
         ep_reward = 0
         obs = env.reset()
         indexes = []
+        obs_x = [obs[0][1]]
+        obs_y = [obs[0][2]]
         while True:
             action = np.random.randint(0, 3)
             # action = np.random.random(2)
             next_obs, reward, done, info = env.step(action)
-            # env.render()
+            env.render()
             obs = next_obs
+            obs_x.append(obs[0][1])
+            obs_y.append(obs[0][2])
             ep_reward += reward
             time.sleep(0.01)
             if done:
                 print("Episodic Reward: %.2f"%ep_reward)
                 break
+
+        # plt.plot(obs_x, obs_y)
+        # plt.show()
+
